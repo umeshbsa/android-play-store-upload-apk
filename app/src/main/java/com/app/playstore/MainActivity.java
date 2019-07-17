fd2b48660298cd3f21e94719b62c999fb9be051a
umeshbsa <umesh.bsa@gmail.com>
2019-07-17 22:19:07 +0530

    Add app center

--- app/src/main/java/com/app/playstore/MainActivity.java
+++ app/src/main/java/com/app/playstore/MainActivity.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AppCenter.start(getApplication(), "0a5e1929-e0e0-4ddc-bf72-c01193c6f889",
+                Analytics.class, Crashes.class);
     }
 }
